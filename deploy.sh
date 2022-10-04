7a7f46a48aa7695689c9bd0504659e1b53db5f08
Gabriel Zhong <gabriel@Gabriels-MacBook-Pro.local>
2022-10-05 00:56:06 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 # install dependencies
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install openjdk-17-jdk docker.io git vim socat nodejs npm
 sudo npm install -g npm@latest
 # build
@@ -16,20 +17,21 @@ cd ..
 mkdir datebase
 sudo docker run --name gc-mongo -p 27017:27017 -v $(pwd)/database:/data/db -d mongo:latest
 # plugins
+mkdir Grasscutter/plugins
 cd Grasscutter/plugins
-sudo wget -c https://github.com/Akiko97/GCAuth/releases/download/v2.5.0-fix/gcauth-2.5.0-fix.jar
-sudo wget -c https://github.com/hamusuke0323/DroppedItemsKiller/releases/download/1.0.1/DroppedItemsKiller.jar
-sudo wget -c https://github.com/Penelopeep/SwitchElementTraveller/releases/download/v5/Switchelement.jar
-sudo wget -c https://github.com/jie65535/gc-opencommand-plugin/releases/download/v1.4.0/opencommand-dev-1.4.0.jar
-sudo wget -c https://github.com/ffauzan/id-look/releases/download/v1.0.3/IdLook-1.0.3.jar
+wget -c https://github.com/Akiko97/GCAuth/releases/download/v2.5.0-fix/gcauth-2.5.0-fix.jar
+wget -c https://github.com/hamusuke0323/DroppedItemsKiller/releases/download/1.0.1/DroppedItemsKiller.jar
+wget -c https://github.com/Penelopeep/SwitchElementTraveller/releases/download/v5/Switchelement.jar
+wget -c https://github.com/jie65535/gc-opencommand-plugin/releases/download/v1.4.0/opencommand-dev-1.4.0.jar
+wget -c https://github.com/ffauzan/id-look/releases/download/v1.0.3/IdLook-1.0.3.jar
     # Without testing
-# sudo wget -c https://github.com/liujiaqi7998/GrasscuttersWebDashboard/releases/download/V6.3.0/GrasscuttersWebDashboard-6.3.0.jar
+# wget -c https://github.com/liujiaqi7998/GrasscuttersWebDashboard/releases/download/V6.3.0/GrasscuttersWebDashboard-6.3.0.jar
     # Deprecated plugins (waiting for update)
-# sudo wget -c https://github.com/exzork/GCAuth/releases/download/v2.5.0/gcauth-2.5.0.jar
-# sudo wget -c https://github.com/Coooookies/Grasscutter-MeaNotice/releases/download/v1.0.3/MeaNotice-1.0-SNAPSHOT.jar
-# sudo wget -c https://github.com/Coooookies/Grasscutter-MeaMailPlus/releases/download/v1.0.3/MeaMailPlus-1.0-SNAPSHOT.jar
-# sudo wget -c https://github.com/gc-toolkit/GLAnnouncement/releases/download/1.0-publish/glannouncement-1.0-SNAPSHOT.jar
-# sudo wget -c https://github.com/gc-mojoconsole/gc-mojoconsole-backend/releases/download/dev-1.4.0/mojoconsole.jar
+# wget -c https://github.com/exzork/GCAuth/releases/download/v2.5.0/gcauth-2.5.0.jar
+# wget -c https://github.com/Coooookies/Grasscutter-MeaNotice/releases/download/v1.0.3/MeaNotice-1.0-SNAPSHOT.jar
+# wget -c https://github.com/Coooookies/Grasscutter-MeaMailPlus/releases/download/v1.0.3/MeaMailPlus-1.0-SNAPSHOT.jar
+# wget -c https://github.com/gc-toolkit/GLAnnouncement/releases/download/1.0-publish/glannouncement-1.0-SNAPSHOT.jar
+# wget -c https://github.com/gc-mojoconsole/gc-mojoconsole-backend/releases/download/dev-1.4.0/mojoconsole.jar
 cd -
 # nginx & cert
 curl https://get.acme.sh | sh -s email=example@example.com
@@ -49,7 +51,7 @@ cd gcauth-web
 npm install
 npm run build
 cd ..
-sudo cp -a gcauth-web/dist/* nginx/html
+cp -a gcauth-web/dist/* nginx/html
 touch nginx/config/nginx.conf
 cat > nginx/config/nginx.conf << EOF
 user  nginx;
@@ -76,8 +78,8 @@ http {
         listen                     443 ssl;
         server_name                www.xxx.com;  #填写绑定证书的域名
 
-        ssl_certificate            /etc/nginx/ssl/xxx.com.pem;  #指定证书的位置，绝对路径
-        ssl_certificate_key        /etc/nginx/ssl/xxx.com.key;  #绝对路径，同上
+        ssl_certificate            /etc/nginx/ssl/cert.pem;  #指定证书的位置，绝对路径
+        ssl_certificate_key        /etc/nginx/ssl/key.pem;  #绝对路径，同上
 
         ssl_session_timeout        5m;
         ssl_protocols              TLSv1 TLSv1.1 TLSv1.2;
